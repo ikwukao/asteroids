8c26e9ec0860e556705621e0d3ba08d1558ab6d7
ikwukao <ikwukao@proton.me>
2026-07-07 17:24:58 +0100

    feat: create asteroids

--- asteroids/main.py
+++ asteroids/main.py
@@ -3,6 +3,8 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -15,13 +17,29 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
+    Player.containers = (
+        updatable,
+        drawable,
+    )
+
+    Asteroid.containers = (
+        asteroids,
+        updatable,
+        drawable,
+    )
+
+    AsteroidField.containers = (
+        updatable,
+    )
 
     player = Player(
         SCREEN_WIDTH / 2,
         SCREEN_HEIGHT / 2,
     )
+
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
