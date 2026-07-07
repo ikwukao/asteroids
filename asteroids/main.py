8d585eaf18aa2b36c1b386f0d9877f6d370f50b0
ikwukao <ikwukao@proton.me>
2026-07-07 12:45:26 +0100

    chore: update and run main.py with uv

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,6 +1,10 @@
+import pygame
+
+
 def main():
-    print("Hello from asteroids!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 
 
 if __name__ == "__main__":
     main()
+
