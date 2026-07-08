eb431632e19e8dac7a3738f030cdc9009b86b3b7
ikwukao <ikwukao@proton.me>
2026-07-08 19:48:11 +0100

    feat: split and reproduce asteroids according to their sizes

--- asteroids/asteroid.py
+++ asteroids/asteroid.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -19,3 +22,32 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+
+    def split(self) -> None:
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+
+        angle = random.uniform(20, 50)
+
+        velocity1 = self.velocity.rotate(angle)
+        velocity2 = self.velocity.rotate(-angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(
+            self.position.x,
+            self.position.y,
+            new_radius,
+        )
+        asteroid1.velocity = velocity1 * 1.2
+
+        asteroid2 = Asteroid(
+            self.position.x,
+            self.position.y,
+            new_radius,
+        )
+        asteroid2.velocity = velocity2 * 1.2
